67a7734def7c6c67de568ca1b6316da6bfa5d0ac
Max <max@froumentin.net>
2014-03-08 12:32:57 +0000

    apply common stylesheets

--- timelines/settings.py
+++ timelines/settings.py
@@ -84,4 +84,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '[CHANGEME absolute path to static files]'
+STATIC_ROOT = '/Users/mf/Desktop/historique/events/static/' #SECRET
