9ae24c4f5ecde3c14fd29f37b1cf9a77756442e7
Max <max@froumentin.net>
2014-04-17 17:21:11 +0100

    Nicer admin

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from events.models import Event, Comment, Person, Link
 
-admin.site.register(Event)
-admin.site.register(Comment)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title','date')
+
+admin.site.register(Event, EventAdmin)
+#admin.site.register(Comment)
 admin.site.register(Person)
 admin.site.register(Link)
 
