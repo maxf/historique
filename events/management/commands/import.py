33475336f7ed9f779758352bc61a405c20df2fdc
Max <max@froumentin.net>
2014-12-19 13:00:45 +0000

    import correctly

--- events/management/commands/import.py
+++ events/management/commands/import.py
@@ -15,19 +15,20 @@ class Command(BaseCommand):
         import_obj = json.loads(sys.stdin.read())
 
         for person in import_obj['people']:
-            Person.objects.create(name=import_obj['people'][person])
+            Person.objects.create(name=import_obj['people'][person]['name'])
 
         for event_obj in import_obj['events']:
             event = Event(title=event_obj['title'])
-            if hasattr(event_obj, 'year'):
-                event['year'] = event_obj['year']
-            if hasattr(event_obj, 'month'):
-                event['month'] = event_obj['month']
-            if hasattr(event_obj, 'day'):
-                event['day'] = event_obj['day']
+            if 'year' in event_obj:
+                event.year = event_obj['year']
+            if 'month' in event_obj:
+                event.month = event_obj['month']
+            if 'day' in event_obj:
+                event.day = event_obj['day']
             event.save()
             participants = []
             for participant in event_obj['participants']:
-                name = import_obj['people'][str(participant)]
-                participants.append(Person.objects.filter(name=name)[0])
+                name = import_obj['people'][str(participant)]['name']
+                people_with_that_name = Person.objects.filter(name=name)
+                participants.append(Person.objects.filter(name__contains=name)[0])
             event.people.add(*participants)
